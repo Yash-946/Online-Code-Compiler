39538c9e3d9d6e859f9fe27c0a4006a389711a0f
Yash-946 <yashagrawal946@gmail.com>
2024-12-09 21:19:47 +0530

    fix chart dates

--- app/api/dashboard/dailyupdate/route.ts
+++ app/api/dashboard/dailyupdate/route.ts
@@ -38,19 +38,44 @@ export async function GET(request: Request) {
       );
     }
 
-    const UserdailyStatusData = await prisma.dailyActivity.findMany({
-      where:{
-        userId:userID
+    const UserDailyStatus = await prisma.dailyActivity.findMany({
+      where: {
+        userId: userID
       }
     })
 
+    const today = new Date();
+    // const year = today.getFullYear();
+    console.log("1")
+    const transformedValues = UserDailyStatus.map((item: any) => ({
+      date: item.date.toISOString().slice(0, 10), // Format to YYYY-MM-DD
+      count: 1,
+    }));
+    console.log("2")
+
+    const startDate = new Date(today.getFullYear(), 0, 1);
+    const endDate = new Date(today.getFullYear()+1, 1, 1);
+
+
+    const allDates = [];
+    for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+      const formattedDate = d.toISOString().slice(0, 10); // Format to YYYY-MM-DD
+      const existingEntry = transformedValues.find(
+        (item: any) => item.date === formattedDate // Compare in the same format
+      );
+      allDates.push({
+        date: formattedDate,
+        count: existingEntry ? existingEntry.count : 0,
+      });
+    }
+
     return NextResponse.json({
       message: "success",
-      Data: UserdailyStatusData
-
+      Data: allDates
     }, { "status": 200 })
 
   } catch (error) {
+    console.log(error)
     return NextResponse.json({
       message: "Fail",
       Error: error
