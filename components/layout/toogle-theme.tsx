ee78c2f904e7c97abd0b450d2d2e1217993f0bf0
Yogesh Pal <yogeshpal5049@gmail.com>
2025-01-05 19:14:17 +0530

    done

--- components/layout/toogle-theme.tsx
+++ components/layout/toogle-theme.tsx
@@ -2,6 +2,8 @@ import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
 import { Moon, Sun } from "lucide-react";
 
+
+
 export const ToggleTheme = () => {
   const { theme, setTheme } = useTheme();
   return (
@@ -21,7 +23,7 @@ export const ToggleTheme = () => {
         <span className="block lg:hidden">Dark</span>
       </div>
 
-      <span className="sr-only">Toggle Theme</span>
+      <span className="sr-only">Toggle-Theme</span>
     </Button>
   );
 };
